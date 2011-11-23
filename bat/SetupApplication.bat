551d9467f8b16d25b44bc7d6ff86edf5bb9f7d27
Arya Antaputra <arya@petewarrior.com>
2011-11-23 09:42:49 +0700

    initial commit

---
+++ bat/SetupApplication.bat
@@ -0,0 +1,46 @@
+
+:: About AIR application packaging
+:: http://livedocs.adobe.com/flex/3/html/help.html?content=CommandLineTools_5.html#1035959
+:: http://livedocs.adobe.com/flex/3/html/distributing_apps_4.html#1037515
+
+:: NOTICE: all paths are relative to project root
+
+:: Your certificate information
+set CERT_NAME="Liquid Metal Mobile"
+set CERT_PASS=fd
+set CERT_FILE="bat\LiquidMetalMobile.p12"
+set SIGNING_OPTIONS=-storetype pkcs12 -keystore %CERT_FILE% -storepass %CERT_PASS%
+
+:: Application descriptor
+set APP_XML=application.xml
+
+:: Files to package
+set APP_DIR=bin
+set FILE_OR_DIR=-C %APP_DIR% .
+
+:: Your application ID (must match <id> of Application descriptor)
+set APP_ID=LiquidMetalMobile
+
+:: Output
+set AIR_PATH=air
+set AIR_NAME=LiquidMetalMobile
+
+:: Debugging using a custom IP
+set DEBUG_IP=
+
+
+:: Validation
+find /C "<id>%APP_ID%</id>" "%APP_XML%" > NUL
+if errorlevel 1 goto badid
+goto end
+
+:badid
+echo.
+echo ERROR: 
+echo   Application ID in 'bat\SetupApplication.bat' (APP_ID) 
+echo   does NOT match Application descriptor '%APP_XML%' (id)
+echo.
+if %PAUSE_ERRORS%==1 pause
+exit
+
+:end
---
+++ bat/SetupSDK.bat
@@ -0,0 +1,21 @@
+
+:: Path to Flex SDK
+set FLEX_SDK=C:\flex_sdk_4.5
+
+
+:: Validation
+if not exist "%FLEX_SDK%" goto flexsdk
+goto succeed
+
+:flexsdk
+echo.
+echo ERROR: incorrect path to Flex SDK in 'bat\SetupSDK.bat'
+echo.
+echo %FLEX_SDK%
+echo.
+if %PAUSE_ERRORS%==1 pause
+exit
+
+:succeed
+set PATH=%PATH%;%FLEX_SDK%\bin
+
